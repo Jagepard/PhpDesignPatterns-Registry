877eba0a996e5da58b5794aa4f9d3496ed4167f0
Jagepard <dankorot@gmail.com>
2018-04-09 16:30:19 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Build Status](https://travis-ci.org/Jagepard/PhpDesignPatterns-Registry.svg?branch=master)](https://travis-ci.org/Jagepard/PhpDesignPatterns-Registry)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/Jagepard/PhpDesignPatterns-Registry/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/Jagepard/PhpDesignPatterns-Registry/?branch=master)
+[![Maintainability](https://api.codeclimate.com/v1/badges/7a6e4faef5f3bedee182/maintainability)](https://codeclimate.com/github/Jagepard/PhpDesignPatterns-Registry/maintainability)
+
+![Registry](https://github.com/Jagepard/PhpDesignPatterns-Registry/blob/master/Registry.png?raw=true "Registry")
+
+
+# Registry
+Реестр 
---
+++ src/Registry.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Date: 09.04.18
+ * Time: 16:11
+ *
+ * @author    : Korotkov Danila <dankorot@gmail.com>
+ * @copyright Copyright (c) 2018, Korotkov Danila
+ * @license   http://www.gnu.org/licenses/gpl.html GNU GPLv3.0
+ */
+
+namespace Structural\Registry;
+
+
+/**
+ * Class Registry
+ * @package Structural\Registry
+ */
+class Registry
+{
+
+    const ALLOWED_KEYS = [
+        'stdClass'
+    ];
+
+    /**
+     * @var array
+     */
+    protected static $data = [];
+
+    /**
+     * @param string $key
+     * @return \stdClass
+     */
+    public static function getData(string $key): \stdClass
+    {
+        if (!array_key_exists($key, self::$data) || !isset(self::$data[$key])) {
+            throw new \InvalidArgumentException('Invalid key given');
+        }
+        
+        return self::$data[$key];
+    }
+
+    /**
+     * @param $key
+     * @param $data
+     */
+    public static function setData(string $key, \stdClass $data)
+    {
+        if (!array_key_exists($key, self::ALLOWED_KEYS)) {
+            throw new \InvalidArgumentException('Invalid key given');
+        }
+
+        self::$data[$key] = $data;
+    }
+}
